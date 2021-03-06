5fcd1b388c4db0ef33a4db829234f7565e29687c
godlycat <ufufhby74@gmail.com>
2021-03-06 19:12:27 +0300

    feat: implement max min avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array||!(array[0]!==undefined && array.every((item)=>typeof(item)==="number"))) return 0;
+  else {
+    let min = array[0]
+    array.map((item)=>{
+      min = min<item ? min : item
+    })
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array||!(array[0]!==undefined && array.every((item)=>typeof(item)==="number"))) return 0;
+  else {
+    let max = array[0]
+    array.map((item)=>{
+      max = max > item ? max : item
+    })
+    return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) { 
+  if(!array||!(array[0]!==undefined && array.every((item)=>typeof(item)==="number"))) return 0
+  else {
+    let sum = 0
+    array.map((item)=>{
+      sum+=item
+    })
+    return sum/(array.length)
+  }
 }
